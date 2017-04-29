62d3c4778b448f489214f3b6ec8b943aebda0074
bfrasure <bill@solutionreach.com>
2017-04-29 11:17:42 -0600

    #time 2h -Flesh out some spacial behavior +breeze lib for linear algebra needs +Cats library for general sweetness -Going to try the Scene update function as my Validation maiden voyage

--- client/src/main/scala/com/billding/Traffic.scala
+++ client/src/main/scala/com/billding/Traffic.scala
@@ -1,30 +1,37 @@
+package com.billding
+
+import cats.data.{NonEmptyList, Validated}
+
 sealed trait Maneuver
+/*
+  Decide how much of a spectrum Braking and Accelerating can use..
+  Version 1 will probably only have 1 hard setting for each.
+ */
 case object Brake extends Maneuver
 case object Accelerate extends Maneuver
 case object Maintain extends Maneuver // Should this also be the move when driver is "cooling down" ?
+/*
+  This will be a slight decrease in speed. To be more accurate, it would be a larger decrease when travelling
+  at a higher speed with increased wind resistance.
+  Probably going to save this for a later version, as a simple simulation can run without this.
+ */
 case object Coast extends Maneuver
 
-trait Point3D
-trait Vec3D
-trait Spatial {
-  val pos: Point3D
+trait WeightedManeuver {
+  val maneuver: Maneuver
+  val urgency: Float
 }
 
 trait Driver {
   val reactionTime: Float
-  val pos: Point3D
+  val spatial: Spatial
 }
 
 trait Vehicle {
-  val speed: Int
+  val spatial: Spatial
   val weight: Int
   val brakingAbility: Float
   // Should this be private and only usable via "vectorBetween" functions and the like?
-  val pos: Point3D
-}
-
-object Spatial {
-  def vecBetween(observer: Spatial, target: Spatial) = ???
 }
 
 trait PilotedVehicle {
@@ -36,6 +43,8 @@ trait PilotedVehicle {
 
 trait Scene {
   def vehicles(): List[PilotedVehicle]
+  val t: Float
+  val dt: Float
 }
 
 trait VehicleSource {
@@ -45,6 +54,7 @@ trait VehicleSource {
   val spacingInDistance: Float
   val spacingInTime: Float
 }
+
 object VehicleSource {
   def withTimeSpacing(averageDt: Float): VehicleSource = ???
   def withDistanceSpacing(averageDpos: Float): VehicleSource = ???
@@ -58,12 +68,20 @@ trait Lane {
 trait Road {
   def lanes: List[Lane]
   def produceVehicles(t: Float)
+  def beginning: Spatial
+  def end: Spatial
 }
 
 trait Universe {
   def calculateDriverResponse(vehicle: PilotedVehicle, scene: Scene): Maneuver
-  def getAllActions(scene: Scene): List[Maneuver]
-  def update(scene: Scene, dt: Float): List[Maneuver]
-  def reactTo(decider: PilotedVehicle, obstacle: Vehicle)
+  def getAllActions(scene: Scene): List[(PilotedVehicle, Maneuver)]
+  /*
+    Consider this as the first use case for Validated.
+   */
+  def update(scene: Scene, dt: Float): Validated[NonEmptyList[String], Scene]
+  def reactTo(decider: PilotedVehicle, obstacle: Spatial): WeightedManeuver
+  def createScene(roads: Road): Scene
+  // Get vehicles that haven't taken a recent action.
+  def reactiveVehicles(scene: Scene): List[PilotedVehicle]
 }
 
