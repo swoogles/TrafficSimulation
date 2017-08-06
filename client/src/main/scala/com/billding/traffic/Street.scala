e81e0de1b44190424e144874ca7fab67f576743e
bfrasure <bill@solutionreach.com>
2017-08-05 21:45:51 -0600

    ScalaRX + ScalaCSS tinkering  #time 1h 40m

--- client/src/main/scala/com/billding/traffic/Street.scala
+++ client/src/main/scala/com/billding/traffic/Street.scala
@@ -16,16 +16,13 @@ object Street {
              beginning: Spatial,
              end: Spatial,
              orientation: Orientation,
-             numLanes: Integer = 1
+             numLanes: Integer
            ): Street = {
 
     val lanes = for (i <- Range(0, numLanes).toList) yield {
       val offset = Meters(6) * i
       val newBeginning = beginning.move(orientation, offset)
       val newEnd = end.move(orientation, offset)
-      pprint.pprintln("newBeginning")
-      pprint.pprintln(newBeginning)
-      val source = VehicleSourceImpl(sourceTiming, newBeginning, newEnd)
       Lane(sourceTiming, newBeginning, newEnd)
     }
     Street(lanes, beginning, end)
--- client/src/main/scala/fr/iscpif/client/Client.scala
+++ client/src/main/scala/fr/iscpif/client/Client.scala
@@ -16,61 +16,124 @@ import squants.motion._
 import squants.space.{Kilometers, LengthUnit, Meters}
 import squants.time.{Milliseconds, Seconds}
 
+import scalacss.internal.Macros.Color
+import scalacss.internal.mutable.{GlobalRegistry, Register, StyleSheet}
+import scalatags.generic.Modifier
+import rx._
+
+import scaladget.tools.JsRxTags._
+import scalatags.JsDom.all._
+import scalacss.ScalatagsCss._
+
+package object client {
+  val CssSettings = scalacss.devOrProdDefaults
+}
+
+import client.CssSettings._
+
+object OutterStyles {
+
+  object TrafficStyles extends StyleSheet.Inline {
+    import dsl._
+
+    val blue: Color = c"#0000FF"
+    val green = c"#00FF00"
+    val currentColor = Var(blue)
+  }
+
+}
+
 /**
   * I think my primary problem with the "collision == death" issue is that I can't start
   * re-accelerating in the desired direction after everything is zeroed out.
   */
 @JSExport("Client")
 object Client {
-  var GLOBAL_T: Time = null
+  var GLOBAL_T: Time = Seconds(0)
 
   val idm: IntelligentDriverModel = new IntelligentDriverModelImpl
   val speedLimit = KilometersPerHour(65)
 
-  def createVehicle(
-                     pIn1: (Double, Double, Double, LengthUnit),
-                     vIn1: (Double, Double, Double, VelocityUnit),
-                      destination: Spatial
-                   ): PilotedVehicle = {
-    PilotedVehicle.commuter(pIn1, vIn1, idm, destination)
-  }
-
   val zeroDimensions: (Double, Double, Double, LengthUnit) = (0, 2, 0, Meters)
-  val originSpatial = Spatial((-50, 0, 0, Meters))
+  val originSpatial = Spatial((0, 0, 0, Meters))
   val endingSpatial = Spatial((100, 0, 0, Kilometers))
-  val originSpatial2 = Spatial((-100, 20, 0, Meters))
-  val endingSpatial2 = Spatial((100, .002, 0, Kilometers))
 
   val herdSpeed = 65
-  /**
-    * TODO: Values should be improved through other means discussed here:
-    * [[com.billding.rendering.CanvasRendering]]
-    */
-
 
   val vehicles: List[PilotedVehicle] = List( )
 
-  val street = Street(Seconds(2), originSpatial, endingSpatial, South, 5)
+  val street = Street(Seconds(2), originSpatial, endingSpatial, South, 1)
 
-//  val lane = Lane(Seconds(1), originSpatial, endingSpatial, vehicles)
-//  val lane2 = Lane(Seconds(2), originSpatial2, endingSpatial2)
   val t = Seconds(0)
   val canvasDimensions: (Length, Length) = (Kilometers(.5), Kilometers(.25))
-  implicit val dt = Milliseconds(20)
+  implicit val DT = Milliseconds(20)
   val scene: Scene = SceneImpl(
     street.lanes,
-//    List(lane, lane2),
     t,
-    dt,
+    DT,
     speedLimit,
     canvasDimensions
   )
+  val carTiming: Var[Time] = Var(Seconds(1))
+
+  val startingColor = modifier(
+    color := "blue"
+  )
+
+  val mods = Var(startingColor)
+  val c = Var("blue")
+  val text = Rx(s"It is a ${c()} text!")
+  val carTimingText = Rx(s"Current car timing ${carTiming()} ")
+
+  import OutterStyles.TrafficStyles
+//  implicit val tolerance = Seconds(.1)
+
+  def updateTiming(): Unit = {
+    carTiming() =
+      if (carTiming.now.approx(Seconds(1)))
+        Seconds(2)
+      else
+        Seconds(1)
+    println("carTiming: " + carTiming.now)
+  }
+  def toggle(): Unit = {
+    TrafficStyles.currentColor() =
+      if (TrafficStyles.currentColor.now == TrafficStyles.blue) TrafficStyles.green else TrafficStyles.blue
+    mods() =
+      if (mods.now == startingColor) {
+        println("toggling from startingColor")
+        modifier(color := "green")
+      }
+      else {
+        println("toggling to startingColor")
+        startingColor
+      }
+
+    c() = if (c.now == "blue") "green" else "blue"
+  }
 
   @JSExport
   def run() {
     val nodes = Seq( )
     val edges = Seq( )
     val millisecondsPerRefresh = 500
+
+    import scalatags.JsDom.all._
+      dom.document.body.appendChild(
+        button(
+          onclick := updateTiming _
+        )(carTimingText).render
+      )
+
+    dom.document.body.appendChild(
+      div(
+//        color := TrafficStyles.currentColor(),
+        color := TrafficStyles.blue.value,
+        onclick := toggle _,
+        text
+      ).render
+    )
+
     var sceneVolatile = scene
     var window = new Window(sceneVolatile, nodes, edges)
     dom.window.setInterval(() => {
@@ -79,7 +142,7 @@ object Client {
         sceneVolatile = sceneVolatile.update(speedLimit)
         window = new Window(sceneVolatile, nodes, edges)
         window.svgNode.forceRedraw()
-    }, dt.toMilliseconds / 5)
+    }, DT.toMilliseconds / 5)
   }
 }
 
