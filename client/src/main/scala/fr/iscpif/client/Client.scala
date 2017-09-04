59452b6a6e163521831fc41c59bf252499f6c4e7
bfrasure <bill@solutionreach.com>
2017-09-04 15:40:46 -0600

    Start splitting out html/callbacks from Client  #time 30m

--- client/src/main/scala/fr/iscpif/client/Client.scala
+++ client/src/main/scala/fr/iscpif/client/Client.scala
@@ -1,18 +1,16 @@
 package client
 
-import java.io.{FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
 
 import com.billding.physics.{South, Spatial}
 import com.billding.traffic._
-import fr.iscpif.client.OutterStyles
 import org.scalajs.dom
 import org.scalajs.dom.html.Input
-import org.scalajs.dom.raw.{HTMLElement, HTMLInputElement, HTMLStyleElement}
+import org.scalajs.dom.raw.{HTMLElement, HTMLInputElement}
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 import squants.{Length, Time}
 
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
 import squants.motion._
 import squants.space.{Kilometers, LengthUnit, Meters}
@@ -20,25 +18,176 @@ import squants.time.{Milliseconds, Seconds}
 import rx._
 
 import scaladget.tools.JsRxTags._
-import scalatags.JsDom.TypedTag
 import scalatags.JsDom.all._
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.parser._
-import io.circe.syntax._
-import io.circe.generic.semiauto._
 import org.scalajs.dom.ext.Ajax
 
-import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 
+case class CreateControlElements(buttonBehaviors: ButtonBehaviors) {
+  def createButtons(element: HTMLElement) = {
+    val columnDiv = div(
+      cls := "col-md-6 text-center"
+    ).render
+    val buttonStyleClasses = "bttn-simple bttn-md bttn-primary lightly-padded"
 
+    columnDiv.appendChild(
+      input(
+        tpe := "button",
+        cls := buttonStyleClasses,
+        value := "Pause",
+        onclick := buttonBehaviors.togglePause
+      ).render
+    )
 
+    columnDiv.appendChild(
+      input(
+        tpe := "button",
+        cls := buttonStyleClasses,
+        value := "Reset the scene!",
+        onclick := buttonBehaviors.initiateSceneReset
+      ).render
+    )
 
-@JSExport("Client")
+    columnDiv.appendChild(
+      input(
+        tpe := "button",
+        cls := buttonStyleClasses,
+        value := "Serialize the scene",
+        onclick := buttonBehaviors.initiateSceneSerialization
+      ).render
+    )
+
+    columnDiv.appendChild(
+      input(
+        tpe := "button",
+        cls := buttonStyleClasses,
+        value := "Deserialize the scene",
+        onclick := buttonBehaviors.initiateSceneDeserialization
+      ).render
+    )
+
+    columnDiv.appendChild(
+      input(
+        cls := buttonStyleClasses,
+        tpe := "button",
+        value := "Disrupt the flow",
+        onclick := buttonBehaviors.toggleDisrupt
+      ).render
+    )
+
+    columnDiv.appendChild(
+      input(
+        cls := buttonStyleClasses,
+        tpe := "button",
+        value := "Disrupt the flow Existing",
+        onclick := buttonBehaviors.toggleDisruptExisting
+      ).render
+    )
+
+    element.appendChild(columnDiv)
+  }
+
+  def createSliders(element: HTMLElement) = {
+    val columnDiv = div(
+      cls := "col-md-6 text-center"
+    )(
+      button(buttonBehaviors.model.carTimingText).render,
+
+      input(
+        tpe := "range",
+        min := 1,
+        max := 10,
+        value := 3,
+        oninput := buttonBehaviors.updateSlider
+      ).render,
+
+      button(
+      )(buttonBehaviors.model.carSpeedText).render,
+
+      input(
+        id := "speedSlider",
+        tpe := "range",
+        min := 20,
+        max := 80,
+        value := 65,
+        step := 5,
+        oninput := buttonBehaviors.speedSliderUpdate
+      ).render
+    ).render
+    element.appendChild(columnDiv)
+  }
+
+}
+
+case class ButtonBehaviors(val model: Model) {
+  val togglePause = (e: dom.Event) => {
+    val elementClicked = e.target.asInstanceOf[HTMLInputElement]
+    elementClicked.value = if (model.paused.now == true) "Pause" else "Unpause"
+    model.paused() = !model.paused.now
+  }
+
+  val toggleDisrupt =
+    (e: dom.Event) => model.disruptLane() = true
+
+  val toggleDisruptExisting =
+    (e: dom.Event) => model.disruptLaneExisting() = true
+
+  val initiateSceneReset =
+    (e: dom.Event) => model.resetScene() = true
+
+  val initiateSceneSerialization =
+    (e: dom.Event) => model.serializeScene() = true
+
+  val initiateSceneDeserialization = (e: dom.Event) => {
+    model.deserializeScene() = true
+  }
+
+  def updateTimingSlider(newTiming: Int): Unit = {
+    model.carTiming() = Seconds(newTiming)
+  }
+
+  def updateSpeedSlider(newTiming: Int): Unit = {
+    model.speed() = KilometersPerHour(newTiming)
+  }
+
+  val updateSlider = (e: dom.Event) => {
+    val value = e.target match {
+      case inputElement: Input  => inputElement.value.toInt
+      case _ => 3 // TODO de-magick this
+    }
+    updateTimingSlider(value)
+  }
+
+  val speedSliderUpdate = (e: dom.Event) => {
+    val value = e.target match {
+      case inputElement: Input  => inputElement.value.toInt
+      case _ => 65 // TODO de-magick this
+    }
+    updateSpeedSlider(value)
+  }
+}
+
+case class Model (
+  val originalScene: Var[SceneImpl],
+  val speed: Var[Velocity] = Var(KilometersPerHour(50)),
+  val carTiming: Var[Time] = Var(Seconds(3)),
+  val paused: Var[Boolean] = Var(false),
+  val disruptLane: Var[Boolean] = Var(false),
+  val disruptLaneExisting: Var[Boolean] = Var(false),
+  val resetScene: Var[Boolean] = Var(false),
+  val serializeScene: Var[Boolean] = Var(false),
+  val deserializeScene: Var[Boolean] = Var(false),
+  val vehicleCount: Var[Int] = Var(0)
+                 ) {
+  val savedScene = Var(originalScene)
+  val carTimingText: Rx.Dynamic[String] = Rx(s"Current car timing ${carTiming()} ")
+  val carSpeedText: Rx.Dynamic[String] = Rx(s"Current car speed ${speed()} ")
+}
+
+@JSExportTopLevel("Client")
 /*
 TODO: Address my concern of traits vs CC's
-  I should keep *all* implemntations out of traits as much as possible
+  I should keep *all* implementations out of traits as much as possible
   Then I avoid the need to explicitly invoke the copy constructor.
 */
 object Client {
@@ -54,6 +203,17 @@ object Client {
   val herdSpeed = 65
 
   val speed = Var(KilometersPerHour(50))
+  val carTiming: Var[Time] = Var(Seconds(3))
+  val carTimingText = Rx(s"Current car timing ${carTiming()} ")
+  val carSpeedText = Rx(s"Current car speed ${speed()} ")
+  val paused = Var(false)
+  val disruptLane = Var(false)
+  val disruptLaneExisting = Var(false)
+  val resetScene: Var[Boolean] = Var(false)
+  val serializeScene = Var(false)
+  val deserializeScene = Var(false)
+  val vehicleCount = Var(0)
+
   val street = Street(Seconds(2), originSpatial, endingSpatial, South, speed.now, 1 )
 
   val t = Seconds(0)
@@ -66,7 +226,11 @@ object Client {
     speedLimit,
     canvasDimensions
   )
-  val carTiming: Var[Time] = Var(Seconds(3))
+  val model = Model(Var(originalScene))
+  val buttonBehaviors = ButtonBehaviors(model)
+  val controlElements = CreateControlElements(buttonBehaviors)
+
+  val savedScene = Var(originalScene)
 
   // Just a snippet to remind me how to pass html parameters around
   val startingColor = modifier(
@@ -75,20 +239,6 @@ object Client {
 
   val car =
     PilotedVehicle.commuter(Spatial.BLANK, new IntelligentDriverModelImpl, Spatial.BLANK)
-
-  val carTimingText = Rx(s"Current car timing ${carTiming()} ")
-  val carSpeedText = Rx(s"Current car speed ${speed()} ")
-
-  val paused = Var(false)
-  val disruptLane = Var(false)
-  val disruptLaneExisting = Var(false)
-  val resetScene = Var(false)
-  val serializeScene = Var(false)
-  val deserializeScene = Var(false)
-
-  val savedScene = Var(originalScene)
-
-  val vehicleCount = Var(0)
 
   def updateTimingSlider(newTiming: Int): Unit = {
     carTiming() = Seconds(newTiming)
@@ -104,21 +254,17 @@ object Client {
     paused() = !paused.now
   }
 
-  val toggleDisrupt = (e: dom.Event) => {
-    disruptLane() = true
-  }
+  val toggleDisrupt =
+    (e: dom.Event) => disruptLane() = true
 
-  val toggleDisruptExisting = (e: dom.Event) => {
-    disruptLaneExisting() = true
-  }
+  val toggleDisruptExisting =
+    (e: dom.Event) => disruptLaneExisting() = true
 
-  val initiateSceneReset = (e: dom.Event) => {
-    resetScene() = true
-  }
+  val initiateSceneReset =
+    (e: dom.Event) => resetScene() = true
 
-  val initiateSceneSerialization = (e: dom.Event) => {
-    serializeScene() = true
-  }
+  val initiateSceneSerialization =
+    (e: dom.Event) => serializeScene() = true
 
   val initiateSceneDeserialization = (e: dom.Event) => {
     deserializeScene() = true
@@ -140,8 +286,6 @@ object Client {
     updateSpeedSlider(value)
   }
 
-  // TODO Should this accept dom.document.body, to make it more obvious what's being mutated?
-  // Also, I think that will make it simple to target other, more specific elements.
   def createButtons(element: HTMLElement) = {
     val columnDiv = div(
       cls := "col-md-6 text-center"
@@ -203,10 +347,8 @@ object Client {
     )
 
     element.appendChild(columnDiv)
-
   }
 
-  // TODO Rearrange these functions so they return the containing div, and are added to the body by the caller
   def createSliders(element: HTMLElement) = {
     val columnDiv = div(
       cls := "col-md-6 text-center"
@@ -252,6 +394,7 @@ object Client {
       id := "button-panel",
       cls := "row"
     ).render
+//    controlElements.createButtons(buttonPanel)
     createButtons(buttonPanel)
     allControls.appendChild(buttonPanel)
     val sliderPanel = div(
@@ -308,13 +451,9 @@ object Client {
       }
       if (serializeScene.now == true) {
         savedScene() = sceneVar.now
-//        println(
-//          sceneVar.now
-//        )
         val f = Ajax.post("http://localhost:8080/writeScene", data=sceneVar.now.toString)
         f.onComplete {
           case Success(xhr) =>
-//            println("response: " + xhr.responseText)
           case Failure(cause) => println("failed: " + cause)
         }
         serializeScene() = false
@@ -323,13 +462,11 @@ object Client {
         val f = Ajax.get("http://localhost:8080/loadScene", data=sceneVar.now.toString)
         f.onComplete {
           case Success(xhr) => {
-            println("response: " + xhr.responseText)
             try {
               val deserializedScene = xhr.responseText.asInstanceOf[SceneImpl]
               // Wish this was working :/
               //              sceneVar() = deserializedScene
               sceneVar() = savedScene.now
-//              println("loaded scene: " + sceneVar.now.streets.flatMap(_.lanes.map(_.vehicles.length)).sum)
               window = new Window(sceneVar.now, nodes, edges)
               window.svgNode.forceRedraw()
               paused() = true
